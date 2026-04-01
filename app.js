f5d040b0db20ccbef14127707b942627062593ab
Samudra-Bose <samudrabose7@gmail.com>
2026-04-02 00:05:06 +0530

    Commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// let board=document.querySelector(".board");
-// const width=  30;
-// const height= 30;
-//  let cols=Math.floor(board.clientWidth/width);
-//  let rows=Math.floor(board.clientHeight/height);
-//  for(let i=0;i<=cols;i++){
-//      for(let j=0;j<=rows;j++){
-// const block=document.createElement("div");
-// block.classList.add("bl");
-// board.appendChild(block);
-//      }
-//  }
+let board=document.querySelector(".board");
+const width=  30;
+const height= 30;
+ let cols=Math.floor(board.clientWidth/width);
+ let rows=Math.floor(board.clientHeight/height);
+ for(let i=0;i<cols;i++){
+     for(let j=0;j<rows;j++){
+const block=document.createElement("div");
+block.classList.add("bl");
+board.appendChild(block);
+     }
+ }
