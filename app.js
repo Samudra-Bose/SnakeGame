ce5a0cdfc8d0d62524f5929ae82e70c4d2887a0b
Samudra-Bose <samudrabose7@gmail.com>
2026-04-07 02:16:39 +0530

    commit

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const width=  60;
 const height= 60;
  let cols=Math.floor(board.clientWidth/width);
  let rows=Math.floor(board.clientHeight/height);
+ let blocks=[];
  for(let i=0;i<rows;i++){
      for(let j=0;j<cols;j++){
     const block=document.createElement("div");
     block.classList.add("bl");
     board.appendChild(block);
     block.innerText=`${i}-${j}`;
-    let blocks;
-    blocks[`${i}-${j}`];
+    blocks[`${i}-${j}`]=block;
      }
  }
  snake=[{
@@ -20,9 +20,20 @@ x:1,y:9
     },{ 
 x:1,y:10
  }];
+ let direction ="left";
 
 function filling(){
     snake.forEach(part=>{
         blocks[`${part.x}-${part.y}`].classList.add("snakebody");
     })
 }
+setInterval(()=>{
+    let head=null;
+    if(direction==="left"){
+        head= {x:snake[0].x,y:snake[0].y-1};
+    }
+    snake.forEach(part=>{
+        blocks[`${part.x}-${part.y}`].classList.add("snakebody");
+    });
+    filling();
+},500);
