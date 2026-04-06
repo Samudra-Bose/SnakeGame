d04aac4991e2c02a44085a862947b5f769b9177a
Samudra-Bose <samudrabose7@gmail.com>
2026-04-07 02:56:47 +0530

    commit

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ const height= 60;
  let cols=Math.floor(board.clientWidth/width);
  let rows=Math.floor(board.clientHeight/height);
  let blocks=[];
- for(let i=0;i<rows;i++){
-     for(let j=0;j<cols;j++){
-    const block=document.createElement("div");
-    block.classList.add("bl");
-    board.appendChild(block);
-    block.innerText=`${i}-${j}`;
-    blocks[`${i}-${j}`]=block;
-     }
- }
  snake=[{
 x:1,y:8
 },];
+let food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
+
+
 let direction ="down";
 window.addEventListener("keydown",(e)=>{
     if(e.key==="ArrowUp"){
@@ -32,13 +26,24 @@ else if(e.key==="ArrowLeft"){
     }
 })
 
+ for(let i=0;i<rows;i++){
+     for(let j=0;j<cols;j++){
+    const block=document.createElement("div");
+    block.classList.add("bl");
+    board.appendChild(block);
+    block.innerText=`${i}-${j}`;
+    blocks[`${i}-${j}`]=block;
+     }
+ }
+
+
 function filling(){
-    snake.forEach(part=>{
-        blocks[`${part.x}-${part.y}`].classList.add("snakebody");
-    })
-}
-setInterval(()=>{
+
+    
+    blocks[`${food.x}-${food.y}`].classList.add("foodcolor");
     let head=null;
+
+
 
     if(direction==="left"){
         head= {x:snake[0].x,y:snake[0].y-1};
@@ -53,11 +58,19 @@ setInterval(()=>{
         head= {x:snake[0].x+1,y:snake[0].y};
     }
 
+
+
     snake.forEach(part=>{
         blocks[`${part.x}-${part.y}`].classList.remove("snakebody");
     });
 
     snake.unshift(head);
     snake.pop();
+    snake.forEach(part=>{
+        blocks[`${part.x}-${part.y}`].classList.add("snakebody");
+    })
+}
+let intervalID=setInterval(()=>{
+    
     filling();
 },300);
