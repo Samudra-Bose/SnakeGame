f158b5ed0bbd211ebd94d6d58f45119ddb77b061
Samudra-Bose <samudrabose7@gmail.com>
2026-04-07 17:40:25 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let board=document.querySelector(".board");
 let startbutton=document.querySelector(".start-btn");
 let startmenu=document.querySelector(".startMenu");
-const width=  60;
-const height= 60;
+const width=  25;
+const height= 25;
  let cols=Math.floor(board.clientWidth/width);
  let rows=Math.floor(board.clientHeight/height);
  let blocks=[];
@@ -14,12 +14,22 @@ let direction ="right";
 
 
 
+// random direction
+// setInterval(() => {
+//     let r=Math.floor(Math.random()*10)
+//     if(r>0 && r<=3) direction="right";
+//     else if(r>3 && r<=5) direction ="up";
+//     else if(r>5 && r<=7) direction ="left";
+//     else if(r>7 && r<=10) direction ="down";
+// }, 1000);
+
+
 for(let i=0;i<rows;i++){
     for(let j=0;j<cols;j++){
         const block=document.createElement("div");
         block.classList.add("bl");
         board.appendChild(block);
-        block.innerText=`${i}-${j}`;
+        // block.innerText=`${i}-${j}`;
         blocks[`${i}-${j}`]=block;
     }
 }
@@ -43,11 +53,11 @@ function filling(){
         head= {x:snake[0].x+1,y:snake[0].y};
     }
     
-    
-    if(head.x<0 || head.x>rows || head.y<0|| head.y>=cols){
-        alert("Game Over");
-        // clearInterval(intervalID);
-    }
+    // || head.x>=rows || head.y<0|| head.y>=cols
+    if(head.x<0 )  head.x=rows-1;
+    else if(head.x>=rows) head.x=0
+    else if(head.y<0 )  head.y=cols-1;
+    else if(head.y>=cols) head.y=0
     
     if(head.x===food.x && head.y===food.y){
         blocks[`${food.x}-${food.y}`].classList.remove("foodcolor");
@@ -86,5 +96,5 @@ startbutton.addEventListener("click",(e)=>{
     startmenu.style.display="none"
     let intervalID=setInterval(()=>{
         filling();
-    },300);
+    },200);
 })
